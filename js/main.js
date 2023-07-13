2b41d6f493b2ec2af873f85dcdc67321fea01c70
dannesx <daniel.viantunes@gmail.com>
2023-07-13 00:04:49 -0300

    Corrigindo detalhes de implementação

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 const menu = document.querySelector('#menu-btn')
 const nav = document.querySelector('header nav')
+const homeLink = document.querySelector('#home-link')
 
 menu.addEventListener('click', () => {
 	nav.classList.toggle('reveal')
+})
+
+window.addEventListener('scroll', () => {
+	let y = window.scrollY
+
+	if (y < 180) {
+		menu.classList.remove('reverse')
+	} else {
+		menu.classList.add('reverse')
+	}
+})
+
+homeLink.addEventListener('click', () => {
+	nav.classList.remove('reveal')
 })
